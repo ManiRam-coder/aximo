8d66fc14842ff87395ed08f4f5c166ca93745329
Mani <chockyapps@gmail.com>
2019-09-25 15:57:42 +0530

    with_reflection

--- mobile/src/main/java/com/axiom/mobile/dao/MobileDAO.java
+++ mobile/src/main/java/com/axiom/mobile/dao/MobileDAO.java
@@ -1,6 +1,8 @@
 package com.axiom.mobile.dao;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.net.URL;
 import java.util.ArrayList;
@@ -11,36 +13,20 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
+import com.axiom.mobile.model.Hardware;
 import com.axiom.mobile.model.Mobile;
+import com.axiom.mobile.model.Release;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 @Repository
 public class MobileDAO {
 
-
 	public List<Mobile> getMobiles(Map<String,String> allParams) throws Exception {
-
-		URL url;
 		List<Mobile> completeMobile = null;
 		List<Mobile> searchMobile = null;
 		try {
-			url = new URL("https://api.myjson.com/bins/1f2r2v?pretty=true");
-			InputStreamReader reader = new InputStreamReader(url.openStream());
-			Gson gson = new Gson();
-			Type founderListType = new TypeToken<ArrayList<Mobile>>() {
-			}.getType();
-			completeMobile = gson.fromJson(reader, founderListType);
-
-			/*	List<Predicate<Mobile>> allPredicatesTest = null;
-			allPredicatesTest = new ArrayList<Predicate<Mobile>>();
-			Predicate<Mobile> batteryPredicate = e -> e.getHardware().getBattery()
-					.equals("Li-Po 7812 mAh battery (29.45 Wh)");
-			String st = "Brand()";
-			Predicate<Mobile> brandPredicate = e -> e.getBrand().equals("Apple");
-			allPredicatesTest.add(brandPredicate);
-			allPredicatesTest.add(batteryPredicate);*/
-			//		allPredicatesTest =  constructMobilePredicate(allParams);
+			completeMobile = getAllMobiles();
 			if(allParams != null && allParams.size() > 0 ) {
 				Predicate<Mobile> compositePredicate1 = constructMobilePredicate(allParams).stream().reduce(w ->true, Predicate::and);
 
@@ -48,94 +34,82 @@ public class MobileDAO {
 						.filter(compositePredicate1).collect(Collectors.toList());
 			}
 			else {
-				return  completeMobile;
+				return completeMobile;
 			}
+
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			throw e;
 		}
 		return searchMobile;
 	}
 
-	/*private List<Predicate<Mobile>> constructMobilePredicate(Map<String,String> allParams) throws ClassNotFoundException {
-		Class<?> cls;
-		List<Predicate<Mobile>> predicateList = new ArrayList<Predicate<Mobile>>();
-		cls = Class.forName(Mobile.class.getName());
-		allParams.forEach((methodName, value) -> {
-			final String methodNameFinal = "get"+methodName;
-			Method[] methods = cls.getMethods(); 
-			try {
-				final Object t = cls.newInstance();
+	private List<Mobile> getAllMobiles() throws Exception{
 
-				Predicate<Mobile>  first= null;
+		try (InputStreamReader reader = new InputStreamReader(new URL("https://api.myjson.com/bins/1f2r2v?pretty=true").openStream())) {
+			Gson gson = new Gson();
+			Type founderListType = new TypeToken<ArrayList<Mobile>>() {}.getType();
+			return gson.fromJson(reader, founderListType);
+		} catch (IOException e) {
+			throw e;
+		}
+	}
 
-				first= e1 -> {
-					try {
-						for (Method method : methods) {
-							if(methodNameFinal.equals(method.getName()))
-							{
-								return method.invoke(e1).equals(value);
-							}
-						}
-					} catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e2) {
-						// TODO Auto-generated catch block
-						e2.printStackTrace();
-					}
-					return false;
-				};
-
-				predicateList.add(first);
-
-			} catch (InstantiationException | IllegalAccessException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		});
-
-		return predicateList;
-	}*/
-	private List<Predicate<Mobile>> constructMobilePredicate(Map<String,String> allParams) {
+	private List<Predicate<Mobile>> constructMobilePredicate(Map<String,String> allParams)throws Exception {
 		List<Predicate<Mobile>> predicateList = new ArrayList<Predicate<Mobile>>();
 		allParams.forEach((methodName, value) -> {
 			Predicate<Mobile>  predicate= null;
-			switch (methodName){
-			case "id":
-				predicate= e -> e.getId().equals(Integer.parseInt(value));
-				break;
-			case "brand":
-				predicate= e -> e.getBrand().equals(value);
-				break;
-			case "phone":
-				predicate= e -> e.getPhone().equals(value);
-				break;
-			case "picture":
-				predicate= e -> e.getPicture().equals(value);
-				break;
-			case "announceDate":
-				predicate= e -> e.getRelease().getAnnounceDate().equals(value);
-				break;
-			case "priceEur":
-				predicate= e -> e.getRelease().getPriceEur().equals(Double.parseDouble(value));
-				break;
-			case "sim":
-				predicate= e -> e.getSim().equals(value);
-				break;
-			case "resolution":
-				predicate= e -> e.getResolution().equals(value);
-				break;
-			case "audioJack":
-				predicate= e -> e.getHardware().getAudioJack().equals(value);
-				break;
-			case "gps":
-				predicate= e -> e.getHardware().getGps().equals(value);
-				break;
-			case "battery":
-				predicate= e -> e.getHardware().getBattery().equals(value);
-				break;
-
-			}
+			predicate = e -> {
+				try {
+					return validateInnerClassRef("get"+methodName, value,e);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				return false;
+			};
 			predicateList.add(predicate);
+
 		});
 		return predicateList;
+	}
+
+	private boolean validateInnerClassRef(String methodName,String value, Mobile e) throws Exception {
+
+		List<String> classNames = new ArrayList<String>();
+		classNames.add("com.axiom.mobile.model.Mobile");
+		classNames.add("com.axiom.mobile.model.Hardware");
+		classNames.add("com.axiom.mobile.model.Release");
+
+		for (String className : classNames) {
+			boolean flag = false;
+			Class<?> cls;
+			try {
+				cls = Class.forName(className);
+
+				Method[] methods = cls.getMethods(); 
+				for (Method method : methods) {
+					if(method.getName().equalsIgnoreCase(methodName))
+					{
+						if(cls.getCanonicalName().equalsIgnoreCase(Mobile.class.getCanonicalName())) {
+							flag = method.getReturnType() == Integer.class ? method.invoke(e).equals(Integer.parseInt(value)) : String.valueOf(method.invoke(e)).toLowerCase().contains(value.toLowerCase());
+							return flag == true;
+						}
+						else if(cls.getCanonicalName().equalsIgnoreCase(Hardware.class.getCanonicalName())) {
+							Object hardware = e.getHardware();
+							flag = method.invoke(hardware).equals(value);
+							return flag == true;
+						}
+						else if(cls.getCanonicalName().equalsIgnoreCase(Release.class.getCanonicalName())) {
+							Object release = e.getRelease();
+							flag = method.getReturnType() == Double.class ? method.invoke(release).equals(Double.parseDouble(value)) : String.valueOf(method.invoke(release)).toLowerCase().contains(value.toLowerCase());
+							return flag == true;
+						}
+					}
+				}
+			} catch (Exception e1) {
+				e1.printStackTrace();
+				throw e1;
+			}
+		}
+		return false;
 	}
 }
